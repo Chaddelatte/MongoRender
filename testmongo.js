d1bf90cb2dec90936197e2f165cd83415cfad1b6
Chaddelatte <145296610+Chaddelatte@users.noreply.github.com>
2026-04-01 11:44:40 -0500

    Fix formatting and clean up testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-constconst user = "web_admin" //process.env.user;
+const user = "web_admin" //process.env.user;
 const paswd = "Access_2_MongoDB" //process.env.paswd;
 const uri = "mongodb+srv://web_admin:Access_2_MongoDB@cmps415-cluster.xhthdtt.mongodb.net/";
 
